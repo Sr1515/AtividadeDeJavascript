e160f7eecec70ce7cebde0880b998f2cfa681339
Sr1515 <joao151576@gmail.com>
2021-07-13 16:19:13 -0300

    foi feito alguns ajustes no formulario e tabela, estava dando erro

---
+++ calculamedia.js
@@ -0,0 +1,58 @@
+var alunos = document.querySelectorAll(".alunos");
+console.log(alunos);
+
+for (var i = 0; i < alunos.length; i++) {
+	var aluno = alunos[i];
+	console.log(i);
+
+	var tdNota1 = aluno.querySelector(".info-n1");
+	var n1 = tdNota1.textContent;
+	console.log(n1);
+
+	var tdNota2 = aluno.querySelector(".info-n2");
+	var n2 = tdNota2.textContent;
+	console.log(n2);
+
+	var tdNota3 = aluno.querySelector(".info-n3");
+	var n3 = tdNota3.textContent;
+	console.log(n3);
+
+	var n1Valido = true;
+	var n2Valido = true;
+	var n3Valido = true;
+
+	if (n1 < 0 || n1 > 100) {
+		console.log("nota é inválida");
+		n1Valido = false;
+		tdNota1.textContent = "Nota é inválida";
+	}
+
+	if (n2 < 0 || n2 > 100) {
+		console.log("nota é inválida");
+		n1Valido = false;
+		tdNota2.textContent = "Nota é inválida";
+	}
+
+	if (n3 < 0 || n3 > 100) {
+		console.log("nota é inválida");
+		n1Valido = false;
+		tdNota3.textContent = "Nota é inválida";
+	}
+
+	if (n1Valido && n2Valido && n3Valido) {
+		var media = calculamedia(
+			parseFloat(n1),
+			parseFloat(n2),
+			parseFloat(n3)
+		);
+		console.log(media);
+
+		var tdMedia_semestral = aluno.querySelector(".info-med");
+		tdMedia_semestral.textContent = media;
+	}
+}
+
+function calculamedia(n1, n2, n3) {
+	let media = (n1 + n2 + n3) / 3;
+	return media.toFixed(2);
+}
---
+++ form.js
@@ -0,0 +1,52 @@
+var botao = document.querySelector("#bt");
+botao.addEventListener("click", function (event) {
+	event.preventDefault();
+
+	var form = document.querySelector("#form-alunos");
+
+	var aluno = obtemAlunoDoForm(form);
+
+	var alunoTr = montaTr(aluno);
+
+	var tabela = document.querySelector("#tabela-alunos");
+	tabela.appendChild(alunoTr);
+
+	form.reset();
+});
+
+function obtemAlunoDoForm(form) {
+	var aluno = {
+		nome: form.nome.value,
+		nota1: form.n1.value,
+		nota2: form.n2.value,
+		nota3: form.n3.value,
+		media: calculamedia(
+			parseFloat(form.n1.value),
+			parseFloat(form.n2.value),
+			parseFloat(form.n3.value)
+		),
+	};
+
+	return aluno;
+}
+
+function montaTd(dado, classe) {
+	var td = document.createElement("td");
+	td.classList.add(classe);
+	td.textContent = dado;
+
+	return td;
+}
+
+function montaTr(aluno) {
+	var alunoTr = document.createElement("tr");
+	alunoTr.classList.add("alunos");
+
+	alunoTr.appendChild(montaTd(aluno.nome, "info-nome"));
+	alunoTr.appendChild(montaTd(aluno.nota1, "info-n1"));
+	alunoTr.appendChild(montaTd(aluno.nota2, "info-n2"));
+	alunoTr.appendChild(montaTd(aluno.nota3, "info-n3"));
+	alunoTr.appendChild(montaTd(aluno.media, "info-med"));
+
+	return alunoTr;
+}
